126041a3c363cea827cc7a16041e51b03694db22
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-11-17 20:41:18 -0500

    Initial Commit

---
+++ Contributions/ContributionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContributionsApp.swift
+//  Contributions
+//
+//  Created by Bryan de Bourbon on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ContributionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
